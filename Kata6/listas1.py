6d7a56df2173534ba82a42b5af9cddfcce189b04
RMN <olearamon3@gmail.com>
2022-02-08 09:52:50 -0600

    cambios

--- Kata6/listas1.py
+++ Kata6/listas1.py
@@ -1,5 +1,5 @@
 planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
 planets.append('Pluton')
 number_of_planets = len(planets)
-print(planets[8],'es el planeta numero', number_of_planets)
+print(planets[-1],'es el planeta numero', number_of_planets)
 
